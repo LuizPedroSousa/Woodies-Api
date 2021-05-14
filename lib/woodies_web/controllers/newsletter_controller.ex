bad98c156ba860d4f776b4ad06c001f9f9b8389b
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-05-14 04:50:47 -0300

    feat(newsletter): add get_all subscribers function :sparkles:

--- lib/woodies_web/controllers/newsletter_controller.ex
+++ lib/woodies_web/controllers/newsletter_controller.ex
@@ -3,6 +3,14 @@ defmodule WoodiesWeb.NewsletterController do
   alias Woodies.{Newsletter}
   action_fallback WoodiesWeb.FallbackController
 
+  def index(conn, _params) do
+    with {:ok, subscribers} <- Woodies.get_all_subscribers() do
+      conn
+      |> put_status(:ok)
+      |> render("index.json", subscribers: subscribers)
+    end
+  end
+
   def subscribe(conn, params) do
     with {:ok, %Newsletter{} = newsletter} <- Woodies.subscribe_newsletter(params) do
       conn
